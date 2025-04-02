f11f6e084d8875cf8b4963ea20c613866e7be224
khanhhy26102002 <ledangkhanhhy@gmail.com>
2025-04-02 14:08:20 +0700

    update file vite

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    proxy: {
-      "/api": {
-        target: "https://bookingpetservice.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
     historyApiFallback: true, // Fix lỗi 404 khi refresh
   },
   plugins: [react()],
